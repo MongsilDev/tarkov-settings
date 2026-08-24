cd1e661a2e2f68a9a513ef5f9540370cd5013067
Mongsil <mail@mongsil.dev>
2026-08-24 17:20:04 +0900

    fix: 설정 저장 위치 LocalAppData로 이동
    구버전 exe 옆 settings.json 마이그레이션 로드

--- Setting/Setting.cs
+++ Setting/Setting.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace tarkov_settings.Setting
@@ -7,16 +8,33 @@ namespace tarkov_settings.Setting
     {
         private const string DEFAULT_FILENAME = "settings.json";
 
-        public void Save(string fileName = DEFAULT_FILENAME) {
-            File.WriteAllText(fileName, JsonConvert.SerializeObject(this, Formatting.Indented));
+        private static readonly string SettingDir = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "tarkov-settings");
+
+        public void Save(string fileName = null)
+        {
+            Directory.CreateDirectory(SettingDir);
+            File.WriteAllText(GetPath(fileName), JsonConvert.SerializeObject(this, Formatting.Indented));
         }
 
-        public static T Load(string fileName = DEFAULT_FILENAME)
+        public static T Load(string fileName = null)
         {
+            string path = GetPath(fileName);
+
+            // migrate from older versions that saved next to the exe
+            if (!File.Exists(path))
+                path = fileName ?? DEFAULT_FILENAME;
+
             T t = new T();
-            if (File.Exists(fileName))
-                t = JsonConvert.DeserializeObject<T>(File.ReadAllText(fileName));
+            if (File.Exists(path))
+                t = JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
             return t;
+        }
+
+        private static string GetPath(string fileName)
+        {
+            return Path.Combine(SettingDir, fileName ?? DEFAULT_FILENAME);
         }
     }
 }
