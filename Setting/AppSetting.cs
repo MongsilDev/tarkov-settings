cbdeb62d79a6a74cc44f7ee0134449a61b196d84
Mongsil <mail@mongsil.dev>
2026-08-25 12:08:32 +0900

    fix: 볼륨 단축키 기본키 PageDown

--- Setting/AppSetting.cs
+++ Setting/AppSetting.cs
@@ -19,7 +19,7 @@ namespace tarkov_settings.Setting
         public string display = @"\\.\DISPLAY1";
         public bool minimizeOnStart = true;
         public bool autostart = false;
-        public string volumeToggleHotkey = "Ctrl+Alt+PageDown";
+        public string volumeToggleHotkey = "PageDown";
         public int volumeLow = 5;
         public int volumeHigh = 100;
     }
